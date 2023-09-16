e2eea80c2b1149718c6cd190a487d76b96618142
Sai krishna <107762536+Saikrishna1504@users.noreply.github.com>
2023-09-16 09:15:43 +0530

    Update exp17_printing_integer_numbers.sh

--- Shell_Programs/exp17_printing_integer_numbers.sh
+++ Shell_Programs/exp17_printing_integer_numbers.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-printDigits() {
-    local num=$1
-    num=${num#-}  # Remove negative sign if present
-    while [ "$num" -gt 0 ]; do
-        echo -n "$((num % 10)) "
-        num=$((num / 10))
-    done
-}
+echo "Enter an integer number: "
+read number
 
-read -p "Enter an integer number: " number
-echo -n "Printing digits of the number: "
-printDigits "$number"
-echo
+echo -n "Digits of the number: "
+
+# Loop to extract and print digits one by one
+while [ $number -ne 0 ]; do
+    digit=$((number % 10)) # Get the last digit
+    echo -n "$digit "      # Print the digit
+    number=$((number / 10)) # Remove the last digit
+done
+
+echo # Print a newline to end the output
